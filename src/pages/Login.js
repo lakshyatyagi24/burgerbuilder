778ed9e7df95e3dff04430c67a56597c5e476630
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-02-24 17:35:02 +0530

    checked login credintials

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import "./Login.css";
+import React, { Component } from "react"; import "./Login.css";
 import user from "../img/2.svg";
 import pass from "../img/3.svg";
 import loginI from "../img/4.svg";
@@ -8,52 +7,57 @@ import forgetI from "../img/6.svg";
 
 class Login extends Component {
 
-    state = {
-        userName:'',
-        password:''
+  checkLogin(e) {
+    e.preventDefault();
+    var usrname = e.target.elements.userName.value;
+    var pswd = e.target.elements.password.value;
+    if(usrname == 'admin' && pswd == 'admin') {
+      alert("Login Successful")
+    }
+    else if(usrname != 'admin' && pswd == 'admin') {
+      alert("User not exist")
+    }
+    else if(usrname == 'admin' && pswd != 'admin') {
+      alert("Password not matched")
+    }
+    else {
+      alert("Wrong Credentials")
     }
 
-    handleChange = (e) => {
-        const { name, value } = e.target 
-        this.setState({ [name]:value })
-    }
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-    }
-
-    render() {
-        return (
-            <div id="main">
-                <div id="left">
-                <div id="login">
-                    <img src = { loginI } alt = "login" id="fourth" /> 
+  render() {
+    return (
+      <div id="main">
+        <div id="left">
+          <div id="login">
+            <img src={loginI} alt="login" id="fourth" />
                     Login
-                </div>
-                <div id="register">
-                    <img src = { registerI } alt = "register" id="fifth" /> 
+          </div>
+          <div id="register">
+            <img src={registerI} alt="register" id="fifth" />
                     Register
-                </div>
-                <div id="forget">
-                    <img src = { forgetI } alt = "forget" id="sixth" /> 
+          </div>
+          <div id="forget">
+            <img src={forgetI} alt="forget" id="sixth" />
                     Forget
-                </div>
-                </div>
-                <form onSubmit = { this.handleSubmit } id="login_page">
-                    <h3 id="loginHere">Login Here</h3>
-                    <div id="imgUser">
-                        <input type="userName" name="userName" placeholder="User Name" id="userName" required />
-                        <img src = {user} alt="user" id="second" />    
-                    </div>
-                    <div id="imgPassword">
-                        <input type="password" name="password" placeholder="Password" id="password" required onChange = { this.handleChange } />
-                        <img src = {pass} alt="password" id="third" />
-                    </div>
-                    <button onSubmit = { this.handleSubmit } id="submit">Log In</button>
-                </form>
-            </div>
-        )
-    }
+          </div>
+        </div>
+        <form method="post" onSubmit={this.checkLogin.bind(this)} id="login_page">
+          <h3 id="loginHere">Login Here</h3>
+          <div id="imgUser">
+            <input type="userName" name="userName" placeholder="User Name" id="userName" required />
+            <img src={user} alt="user" id="second" />
+          </div>
+          <div id="imgPassword">
+            <input type="password" name="password" placeholder="Password" id="password" required />
+            <img src={pass} alt="password" id="third" />
+          </div>
+          <button onSubmit={this.LoginCheck} id="submit">Log In</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Login;
