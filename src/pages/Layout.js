27820bcee970f0318956a1961640fd85b13d2002
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-03-03 22:12:43 +0530

    BurgerIngredient completed

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,12 @@
+import classes from "*.module.css";
 import React from "react";
 import Aux from "./Aux";
+import classes from "./Layout.css";
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={ classes.Content }>
             { props.children }
         </main>
     </Aux>
