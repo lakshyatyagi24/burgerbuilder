5a958fba70251f5d6fb9f7f006418fb30bbccf44
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-03-03 23:00:21 +0530

    .

--- src/pages/BurgerBuilder.js
+++ src/pages/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import Aux from "./Aux";
+import layout from "./Layout";
 
 class BurgerBuilder extends Component {
     render () {
         return(
-            <Aux>
-                
-            </Aux>
+            <div>
+                <layout />
+            </div>
         );
     }
 }
