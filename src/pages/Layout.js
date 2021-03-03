ff483189798761ce105bae46e8a93b411bc47b7d
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-03-03 23:06:00 +0530

    classes error fixed

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,7 @@
 import classes from "*.module.css";
 import React from "react";
 import Aux from "./Aux";
-import classes from "./Layout.css";
+// import classes from "./Layout.css";
 
 const layout = (props) => (
     <Aux>
