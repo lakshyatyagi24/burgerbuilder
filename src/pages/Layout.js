c059301082291c926f8da2f561b5fac9aa268d3a
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-03-03 23:09:27 +0530

    fixed module error

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,6 @@
-import classes from "*.module.css";
 import React from "react";
 import Aux from "./Aux";
-// import classes from "./Layout.css";
+import classes from "./Layout.css";
 
 const layout = (props) => (
     <Aux>
