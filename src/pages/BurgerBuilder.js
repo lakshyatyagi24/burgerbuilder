52ab1746fe440daaf1641c845f83c89573ca2e95
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-03-04 22:04:25 +0530

    buildcontrols

--- src/pages/BurgerBuilder.js
+++ src/pages/BurgerBuilder.js
@@ -3,10 +3,24 @@ import Aux from "./Aux";
 import Burger from "./Burger/Burger";
 
 class BurgerBuilder extends Component {
+
+    // constructor( props ) {
+    //     super( props );
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render () {
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients = { this.state.ingredients } />
                 <div>Build Controls</div>
             </Aux>
         );
